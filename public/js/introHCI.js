be793a91bd720c15ee31ace1e0f12b736ad12ddf
Thuy Nguyen <dtn073@ucsd.edu>
2017-02-23 00:39:22 -0800

    pushed

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,4 +10,9 @@ $(document).ready(function() {
  */
 function initializePage() {
 	// your code here
+	$(".likeBtn").click(detectLike);
+}
+
+function detectLike(e){
+	ga("send", "event", 'like', 'click');
 }
